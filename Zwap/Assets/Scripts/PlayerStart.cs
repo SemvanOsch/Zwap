e38cea199f9663d5902a09f1b347cf49fda75d98
sildatema <145674582+sildatema@users.noreply.github.com>
2026-09-04 10:19:09 +0200

    stepup to fish collision

--- Zwap/Assets/Scripts/PlayerStart.cs
+++ Zwap/Assets/Scripts/PlayerStart.cs
@@ -54,18 +54,16 @@ public class PlayerStart : MonoBehaviour
         Vector3 move = new Vector3(combined.x, combined.y, 0f) * moveSpeed;
         rb.MovePosition(transform.position + move);
     }
-}
-
-public class PlayerCollision : MonoBehaviour
-{
-    void OnCollisionEnter2D(Collision2D collision)
+    
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Collided with: " + collision.gameObject.name);
+        Debug.Log("Collided with: " + other.gameObject.name);
 
-        if (collision.gameObject.CompareTag("Rock"))
+        if (other.gameObject.CompareTag("Entity"))
         {
             Debug.Log("Tag matched, loading scene");
             SceneManager.LoadScene("Home-Screen");
         }
     }
 }
+
