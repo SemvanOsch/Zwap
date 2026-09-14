3a17ae472dac642c2d00b7dedbcb6e66439b0e20
sildatema <145674582+sildatema@users.noreply.github.com>
2026-09-14 16:18:07 +0200

    last fix

--- Zwap/Assets/Scripts/Scene-manager.cs
+++ Zwap/Assets/Scripts/Scene-manager.cs
@@ -7,6 +7,10 @@ public class SceneButtonHandler : MonoBehaviour
 
     public void GoToScene()
     {
+        // Make sure time/audio aren't left frozen from a paused state
+        Time.timeScale = 1f;
+        AudioListener.pause = false;
+
         SceneManager.LoadScene(sceneToLoad);
     }
 }
